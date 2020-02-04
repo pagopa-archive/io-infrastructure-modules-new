079d561f5615dbf212e5bdc7efede45140ff3dc6
Francesco Persico <francescopersico@gmail.com>
2020-02-04 20:01:52 +0100

    [#171085424] Add module application_insights

---
+++ azurerm_application_insights/vars.tf
@@ -0,0 +1,33 @@
+variable "global_prefix" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "retention_in_days" {
+  type    = number
+  default = 180
+}
+
+variable "application_type" {
+  type    = string
+  default = "other"
+}
+
+locals {
+  resource_name = "${var.global_prefix}-${var.environment}-ai-${var.name}"
+}
