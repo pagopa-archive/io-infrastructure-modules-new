887c3869fa7228b70fd72d8dc4096189c1e0959f
Francesco Persico <francescopersico@gmail.com>
2020-02-04 15:36:52 +0100

    [#171078480] Add module virtual_network (#3)

---
+++ azurerm_virtual_network/outputs.tf
@@ -0,0 +1,11 @@
+output "id" {
+  value = azurerm_virtual_network.virtual_network.id
+}
+
+output "resource_group_name" {
+  value = var.resource_group_name
+}
+
+output "resource_name" {
+  value = local.resource_name
+}
