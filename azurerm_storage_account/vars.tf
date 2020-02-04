e1552d9dd57eb51048871ce7239ddd0e1df997b9
Francesco Persico <francescopersico@gmail.com>
2020-02-04 13:46:47 +0100

    [#171077597] Add module storage_account (#2)

---
+++ azurerm_storage_account/vars.tf
@@ -0,0 +1,40 @@
+variable "global_prefix" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "account_kind" {
+  type    = string
+  default = "StorageV2"
+}
+
+variable "account_tier" {
+  type = string
+}
+
+variable "account_replication_type" {
+  type = string
+}
+
+variable "access_tier" {
+  type = string
+}
+
+locals {
+  resource_name = "${var.global_prefix}${var.environment}st${var.name}"
+}
