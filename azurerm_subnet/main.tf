5fce2435fa22c092888fe19d56f3e53b0a0926e5
Francesco Persico <francescopersico@gmail.com>
2020-02-05 19:12:26 +0100

    [#171110661] Add module subnet (#9)

---
+++ azurerm_subnet/main.tf
@@ -0,0 +1,15 @@
+provider "azurerm" {
+  version = "=1.42.0"
+}
+
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "azurerm" {}
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = local.resource_name
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = var.virtual_network_name
+  address_prefix       = var.address_prefix
+}
---
+++ azurerm_subnet/outputs.tf
@@ -0,0 +1,7 @@
+output "id" {
+  value = azurerm_subnet.subnet.id
+}
+
+output "name" {
+  value = azurerm_subnet.subnet.id
+}
