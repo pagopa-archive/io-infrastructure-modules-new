b628d9c02f8307c63625e6ced9a64f2df046e742
Roberto Chessa <r.chessa@beetobit.com>
2020-02-05 23:51:33 +0100

    [#171102346] container group module and primary key output for storage_account

--- azurerm_storage_account/outputs.tf
+++ azurerm_storage_account/outputs.tf
@@ -15,3 +15,8 @@ output "primary_access_key" {
 output "resource_name" {
   value = local.resource_name
 }
+
+output "primary_access_key" 
+  value = azurerm_storage_account.storage_account.primary_access_key
+  sensitive = true
+}
