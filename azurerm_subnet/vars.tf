34beebc9571a0e4100cdac81d5e391b7907baceb
Francesco Persico <francescopersico@gmail.com>
2020-02-05 19:09:54 +0100

    [#171110661] Add module subnet

---
+++ azurerm_subnet/vars.tf
@@ -0,0 +1,31 @@
+variable "global_prefix" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "virtual_network_name" {
+  type = string
+}
+
+variable "address_prefix" {
+  type = string
+}
+
+locals {
+  resource_name = "${var.global_prefix}-${var.environment}-snet-${var.name}"
+}
