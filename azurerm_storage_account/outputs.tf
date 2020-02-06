3fcbf06b80b20dbd29e8b7ee1ad195c7e7cb56bd
Francesco Persico <francescopersico@gmail.com>
2020-02-06 15:02:32 +0100

    [#171127631] Module storage_account: add primary_acces_key output (#12)

--- azurerm_storage_account/outputs.tf
+++ azurerm_storage_account/outputs.tf
@@ -7,6 +7,11 @@ output "primary_connection_string" {
   sensitive = true
 }
 
+output "primary_access_key" {
+  value     = azurerm_storage_account.storage_account.primary_access_key
+  sensitive = true
+}
+
 output "resource_name" {
   value = local.resource_name
 }
