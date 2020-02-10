0247b397a6cee89ebe864a4f2355cf700326cda3
Roberto Chessa <r.chessa@beetobit.com>
2020-02-10 22:02:48 +0100

    [#171102346] remove storage_account primary key output

--- azurerm_storage_account/outputs.tf
+++ azurerm_storage_account/outputs.tf
@@ -15,8 +15,3 @@ output "primary_access_key" {
 output "resource_name" {
   value = local.resource_name
 }
-
-output "primary_access_key" 
-  value = azurerm_storage_account.storage_account.primary_access_key
-  sensitive = true
-}
