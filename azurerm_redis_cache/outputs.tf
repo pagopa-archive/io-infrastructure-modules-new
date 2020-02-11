eaccddfbc75df339fda594b64b0e2cf52bb14f5c
Walter Traspadini <walter.traspadini@sorint.it>
2020-02-11 17:53:25 +0100

    [#171179503] refactoring output redis module

--- azurerm_redis_cache/outputs.tf
+++ azurerm_redis_cache/outputs.tf
@@ -1,7 +1,11 @@
+output "id" {
+  value = azurerm_redis_cache.redis_cache.id
+}
+
 output "name" {
   value = azurerm_redis_cache.redis_cache.name
 }
 
-output "access_key" {
+output "primary_access_key" {
   value = azurerm_redis_cache.redis_cache.primary_access_key
 }
