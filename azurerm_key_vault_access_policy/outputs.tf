3ca6d3d1336d1e9de94429f6931a8f701490924e
Walter Traspadini <walter.traspadini@sorint.it>
2020-02-11 16:58:35 +0100

    [#171210559] key vault access policy module

---
+++ azurerm_key_vault_access_policy/outputs.tf
@@ -0,0 +1,11 @@
+output "id" {
+  value = azurerm_key_vault_access_policy.key_vault_access_policy.id
+}
+
+output "name" {
+  value = azurerm_key_vault_access_policy.key_vault_access_policy.vault_name
+}
+
+output "resource_group_name" {
+  value = azurerm_key_vault_access_policy.key_vault_access_policy.resource_group_name
+}
