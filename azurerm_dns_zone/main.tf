71f6d81bb3b1c7b77c4c215de3d81fe2df735c87
Roberto Chessa <r.chessa@beetobit.com>
2020-02-11 13:14:15 +0100

    [#171207418] dns_zone first import

---
+++ azurerm_dns_zone/main.tf
@@ -0,0 +1,17 @@
+provider "azurerm" {
+  version = "~>1.43"
+}
+
+terraform {
+  backend "azurerm" {}
+}
+
+# New infrastructure
+resource "azurerm_dns_zone" "dns_zone" {
+  name                = var.dns_zone_name
+  resource_group_name = var.resource_group_name
+
+  tags = {
+    environment = var.environment
+  }
+}
---
+++ azurerm_dns_zone/outputs.tf
@@ -0,0 +1,14 @@
+output "dns_zone_name" {
+  description = "The full DNS name of the new zone."
+  value       = var.dns_zone_name
+}
+
+output "dns_zone_id" {
+  description = "The full DNS name of the new zone."
+  value       = azurerm_dns_zone.dns_zone.id
+}
+
+output "dns_zone_nameservers" {
+  description = "The list of name servers used for the zone."
+  value       = azurerm_dns_zone.dns_zone.name_servers
+}
