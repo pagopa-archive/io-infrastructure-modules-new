d4e99fd6e7da1c86f4ef92fdabcb0f63d440474f
Walter Traspadini <wtraspad@yahoo.it>
2020-02-12 09:55:12 +0100

    [#171210559] Key Vault Access Policy Module (#18)
    * [#171210559] key vault access policy module
    
    * [#171210559] formatting as suggested in the rievieew

---
+++ azurerm_key_vault_access_policy/main.tf
@@ -0,0 +1,19 @@
+provider "azurerm" {
+  version = "=1.42.0"
+}
+
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "azurerm" {}
+}
+
+resource "azurerm_key_vault_access_policy" "key_vault_access_policy" {
+  key_vault_id            = var.key_vault_id
+  tenant_id               = var.tenant_id
+  object_id               = var.object_id
+  application_id          = var.application_id
+  key_permissions         = var.key_permissions
+  secret_permissions      = var.secret_permissions
+  certificate_permissions = var.certificate_permissions
+  storage_permissions     = var.storage_permissions
+}
---
+++ azurerm_key_vault_access_policy/vars.tf
@@ -0,0 +1,42 @@
+variable "key_vault_id" {
+  type        = string
+  description = "Specifies the id of the Key Vault resource."
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault."
+}
+
+variable "object_id" {
+  type        = string
+  description = "The object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
+}
+
+variable "application_id" {
+  type        = string
+  description = "The object ID of an Application in Azure Active Directory."
+  default     = null
+}
+
+variable "key_permissions" {
+  type        = list(string)
+  description = "List of key permissions."
+}
+
+variable "secret_permissions" {
+  type        = list(string)
+  description = "List of secret permissions."
+}
+
+variable "certificate_permissions" {
+  type        = list(string)
+  description = "List of certificate permissions."
+  default     = null
+}
+
+variable "storage_permissions" {
+  type        = list(string)
+  description = "List of storage permissions."
+  default     = null
+}
