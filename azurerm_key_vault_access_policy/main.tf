0578e849f16f24746f90208ce9ed2ce4769fb5cb
Walter Traspadini <walter.traspadini@sorint.it>
2020-02-12 08:52:22 +0100

    [#171210559] formatting as suggested in the rievieew

--- azurerm_key_vault_access_policy/main.tf
+++ azurerm_key_vault_access_policy/main.tf
@@ -8,14 +8,12 @@ terraform {
 }
 
 resource "azurerm_key_vault_access_policy" "key_vault_access_policy" {
-  key_vault_id              = var.key_vault_id
-  tenant_id                 = var.tenant_id
-  object_id                 = var.object_id
-  application_id            = var.application_id
-
-  key_permissions           = var.key_permissions
-  secret_permissions        = var.secret_permissions
-  certificate_permissions   = var.certificate_permissions
-  storage_permissions       = var.storage_permissions
-
+  key_vault_id            = var.key_vault_id
+  tenant_id               = var.tenant_id
+  object_id               = var.object_id
+  application_id          = var.application_id
+  key_permissions         = var.key_permissions
+  secret_permissions      = var.secret_permissions
+  certificate_permissions = var.certificate_permissions
+  storage_permissions     = var.storage_permissions
 }
