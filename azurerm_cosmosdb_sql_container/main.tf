7ed44e71b6385dd92c0d095859dbb09a84f66ad5
Walter Traspadini <wtraspad@yahoo.it>
2020-02-12 11:19:47 +0100

    [#171207601] Create module cosmosdb_sql_container (#17)
    * [#171207601] new module cosmosdb container
    
    * [#171207601] fix name variable description
    
    * [#171207601] code formatting agreed within the review
    
    * [#171207601] removed prefix and env in database name
    
    * [#171207601] directory renamed
    
    * [#171207601] dynamic block unique_key

---
+++ azurerm_cosmosdb_sql_container/main.tf
@@ -0,0 +1,28 @@
+provider "azurerm" {
+  version = "=1.42.0"
+}
+
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "azurerm" {}
+}
+
+resource "azurerm_cosmosdb_sql_container" "cosmosdb_sql_container" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+
+  account_name       = var.account_name
+  database_name      = var.database_name
+  partition_key_path = var.partition_key_path
+  throughput         = var.throughput
+  default_ttl        = var.default_ttl
+
+  dynamic "unique_key" {
+    for_each = var.unique_key_paths
+      content {
+        paths = [unique_key.value]
+     }
+  }
+}
+
+
---
+++ azurerm_cosmosdb_sql_container/vars.tf
@@ -0,0 +1,53 @@
+variable "global_prefix" {
+  type        = string
+  description = "Prefix used to define the resource name."
+}
+
+variable "environment" {
+  type        = string
+  description = "The name of the environment"
+}
+
+variable "name" {
+  type        = string
+  description = "The name of the Cosmos DB instance."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "The name of the resource group in which the Cosmos DB SQL"
+}
+
+variable "account_name" {
+  type        = string
+  description = "The name of the Cosmos DB Account to create the container within."
+}
+
+variable "database_name" {
+  type        = string
+  description = "The name of the Cosmos DB SQL Database to create the container within."
+}
+
+variable "partition_key_path" {
+  type        = string
+  description = "Define a partition key."
+  default     = null
+}
+
+variable "throughput" {
+  type        = number
+  description = "The throughput of SQL container (RU/s). Must be set in increments of 100. The minimum value is 400."
+  default     = null
+}
+
+variable "default_ttl" {
+  type        = number
+  description = "The default time to live of SQL container. If missing, items are not expired automatically."
+  default     = null
+}
+
+variable "unique_key_paths" {
+  type        = list(string)
+  description = "A list of paths to use for this unique key."
+  default     = []
+}
