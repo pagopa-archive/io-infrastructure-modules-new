1d30ebc210ca722be267c07325f711e4f31ede19
Walter Traspadini <walter.traspadini@sorint.it>
2020-02-12 14:01:04 +0100

    [#171227816] module azurerm_cosmosdb_sql_database first commit

---
+++ azurerm_cosmosdb_sql_database/main.tf
@@ -0,0 +1,15 @@
+provider "azurerm" {
+  version = "=1.42.0"
+}
+
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "azurerm" {}
+}
+
+resource "azurerm_cosmosdb_sql_database" "cosmosdb_sql_database" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  account_name        = var.account_name
+  throughput          = var.throughput
+}
---
+++ azurerm_cosmosdb_sql_database/outputs.tf
@@ -0,0 +1,7 @@
+output "id" {
+  value = azurerm_cosmosdb_sql_database.cosmosdb_sql_database.id
+}
+
+output "name" {
+  value = azurerm_cosmosdb_sql_database.cosmosdb_sql_database.name
+}
