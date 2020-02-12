46de6fc338f3c0161e6c8aff01e8e885c3fe4fd9
Roberto Chessa <r.chessa@beetobit.com>
2020-02-12 09:49:30 +0100

    [#171207418] remove dns_zone_  from output

--- azurerm_dns_zone/outputs.tf
+++ azurerm_dns_zone/outputs.tf
@@ -1,14 +1,14 @@
-output "dns_zone_name" {
+output "domain_name" {
   description = "The full DNS name of the new zone."
   value       = var.dns_zone_name
 }
 
-output "dns_zone_id" {
+output "id" {
   description = "The full DNS name of the new zone."
   value       = azurerm_dns_zone.dns_zone.id
 }
 
-output "dns_zone_nameservers" {
+output "nameservers" {
   description = "The list of name servers used for the zone."
   value       = azurerm_dns_zone.dns_zone.name_servers
 }
