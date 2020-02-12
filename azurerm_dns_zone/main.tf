61cac334542dd2d41c3fcf7d88cf98dea9452b7f
Francesco Persico <francescopersico@gmail.com>
2020-02-12 14:55:34 +0100

    [#171207418] Fix variable name

--- azurerm_dns_zone/main.tf
+++ azurerm_dns_zone/main.tf
@@ -8,7 +8,7 @@ terraform {
 
 # New infrastructure
 resource "azurerm_dns_zone" "dns_zone" {
-  name                = var.dns_zone_name
+  name                = var.name
   resource_group_name = var.resource_group_name
 
   tags = {
--- azurerm_dns_zone/vars.tf
+++ azurerm_dns_zone/vars.tf
@@ -16,7 +16,7 @@ variable "resource_group_name" {
   type = string
 }
 
-variable "dns_zone_name" {
+variable "name" {
   type        = string
   description = "The name of the DNS Zone. Must be a valid domain name."
 }
