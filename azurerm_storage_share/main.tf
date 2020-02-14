13d5b5ffb8e2dadb24d1af67b12d28d21aae8744
Francesco Persico <francescopersico@gmail.com>
2020-02-14 10:10:24 +0100

    [#171274380] Add module_depends_on for storage_share (#25)

--- azurerm_storage_share/main.tf
+++ azurerm_storage_share/main.tf
@@ -8,6 +8,8 @@ terraform {
 }
 
 resource "azurerm_storage_share" "storage_share" {
+  depends_on = [var.module_depends_on]
+
   name                 = var.name
   storage_account_name = var.storage_account_name
   quota                = var.quota
