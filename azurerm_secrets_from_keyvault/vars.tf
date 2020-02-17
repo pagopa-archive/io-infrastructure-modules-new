510f91014d3c31ece657072d1f9e8f416711272f
Francesco Persico <francescopersico@gmail.com>
2020-02-17 12:59:05 +0100

    [#171308045] Remove unused variables from secrets_from_keyvault

--- azurerm_secrets_from_keyvault/vars.tf
+++ azurerm_secrets_from_keyvault/vars.tf
@@ -1,23 +1,3 @@
-variable "global_prefix" {
-  type = string
-}
-
-variable "environment" {
-  type = string
-}
-
-variable "region" {
-  type = string
-}
-
-variable "name" {
-  type = string
-}
-
-variable "resource_group_name" {
-  type = string
-}
-
 variable "key_vault_id" {
   type = string
 }
