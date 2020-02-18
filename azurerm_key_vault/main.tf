e34b972ff5021a5f3bcc6f9fb5886fc45d9d6663
Francesco Persico <francescopersico@gmail.com>
2020-02-18 10:11:17 +0100

    [#171103493] Add key_vault module

---
+++ azurerm_key_vault/main.tf
@@ -0,0 +1,20 @@
+provider "azurerm" {
+  version = "=1.42.0"
+}
+
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "azurerm" {}
+}
+
+resource "azurerm_key_vault" "key_vault" {
+  name                = local.resource_name
+  location            = var.region
+  resource_group_name = var.resource_group_name
+  tenant_id           = local.tenant_id
+  sku_name            = var.sku_name
+
+  tags = {
+    environment = var.environment
+  }
+}
