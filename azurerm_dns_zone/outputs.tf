b6ff45cfee613d1a139fe2ee42d531edb0730336
Francesco Persico <francescopersico@gmail.com>
2020-02-19 11:56:46 +0100

    [#171352375] Add resource_group_name for dns_zone (#34)

--- azurerm_dns_zone/outputs.tf
+++ azurerm_dns_zone/outputs.tf
@@ -12,3 +12,7 @@ output "nameservers" {
   description = "The list of name servers used for the zone."
   value       = azurerm_dns_zone.dns_zone.name_servers
 }
+
+output "resource_group_name" {
+  value = var.resource_group_name
+}
