067ad0cc92c9b8361531a9c7b1dc83063711d18f
Walter Traspadini <walter.traspadini@sorint.it>
2020-02-26 23:23:30 +0100

    #171491898 formatting

--- azurerm_linux_virtual_machine/main.tf
+++ azurerm_linux_virtual_machine/main.tf
@@ -16,15 +16,15 @@ resource "azurerm_network_security_group" "network_security_group" {
   dynamic "security_rule" {
     for_each = var.security_rules
     content {
-      name                          = security_rule.value.name
-      priority                      = security_rule.value.priority
-      direction                     = security_rule.value.direction
-      access                        = security_rule.value.access
-      protocol                      = security_rule.value.protocol
-      source_port_ranges            = security_rule.value.source_port_ranges
-      destination_port_ranges       = security_rule.value.destination_port_ranges
-      source_address_prefixes        = security_rule.value.source_address_prefixes
-      destination_address_prefixes  = security_rule.value.destination_address_prefixes
+      name                         = security_rule.value.name
+      priority                     = security_rule.value.priority
+      direction                    = security_rule.value.direction
+      access                       = security_rule.value.access
+      protocol                     = security_rule.value.protocol
+      source_port_ranges           = security_rule.value.source_port_ranges
+      destination_port_ranges      = security_rule.value.destination_port_ranges
+      source_address_prefixes      = security_rule.value.source_address_prefixes
+      destination_address_prefixes = security_rule.value.destination_address_prefixes
     }
   }
 
@@ -34,9 +34,9 @@ resource "azurerm_network_security_group" "network_security_group" {
 }
 
 resource "azurerm_network_interface" "network_interface" {
-  name                      = "nic-${var.name}"
-  location                  = var.region
-  resource_group_name       = var.resource_group_name
+  name                = "nic-${var.name}"
+  location            = var.region
+  resource_group_name = var.resource_group_name
 
   ip_configuration {
     name                          = "snet-${var.name}"
