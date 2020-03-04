a92eaa09932e2deba04b224348687208639e06a9
Walter Traspadini <walter.traspadini@pagopa.it>
2020-03-04 08:59:32 +0100

    [#171596209] Redis Module hostname and ports as outputs (#51)
    * [#171596209] hostname and ssl port as output
    
    * [#171596209] formatting

--- azurerm_redis_cache/outputs.tf
+++ azurerm_redis_cache/outputs.tf
@@ -9,3 +9,15 @@ output "name" {
 output "primary_access_key" {
   value = azurerm_redis_cache.redis_cache.primary_access_key
 }
+
+output "hostname" {
+  value = azurerm_redis_cache.redis_cache.hostname  
+}
+
+output "port" {
+  value = azurerm_redis_cache.redis_cache.port
+}
+
+output "ssl_port" {
+  value = azurerm_redis_cache.redis_cache.ssl_port
+}
