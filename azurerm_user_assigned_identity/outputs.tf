ea03ef10ab93a438873fc1730b4fc767dae4d3a4
Francesco Persico <francescopersico@gmail.com>
2020-03-08 16:08:15 +0100

    [#171686534] Module user_assigned_identity (#56)

---
+++ azurerm_user_assigned_identity/outputs.tf
@@ -0,0 +1,15 @@
+output "id" {
+  value = azurerm_user_assigned_identity.user_assigned_identity.id
+}
+
+output "name" {
+  value = azurerm_user_assigned_identity.user_assigned_identity.name
+}
+
+output "principal_id" {
+  value = azurerm_user_assigned_identity.user_assigned_identity.principal_id
+}
+
+output "client_id" {
+  value = azurerm_user_assigned_identity.user_assigned_identity.client_id
+}
