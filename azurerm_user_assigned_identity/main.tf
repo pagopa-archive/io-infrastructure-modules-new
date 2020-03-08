3f84788c1b384c89312f916e9c7d5715491f1a9a
Francesco Persico <francescopersico@gmail.com>
2020-03-08 16:07:18 +0100

    [#171686534] Module user_assigned_identity

---
+++ azurerm_user_assigned_identity/main.tf
@@ -0,0 +1,18 @@
+provider "azurerm" {
+  version = "=1.44.0"
+}
+
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "azurerm" {}
+}
+
+resource "azurerm_user_assigned_identity" "user_assigned_identity"{
+  name                = local.resource_name
+  resource_group_name = var.resource_group_name
+  location            = var.region
+
+  tags = {
+    environment = var.environment
+  }
+}
---
+++ azurerm_user_assigned_identity/vars.tf
@@ -0,0 +1,31 @@
+variable "global_tenant_id" {
+  type = string
+}
+
+variable "global_prefix" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "environment_short" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+locals {
+  resource_name = "${var.global_prefix}-${var.environment_short}-uai-${var.name}"
+}
