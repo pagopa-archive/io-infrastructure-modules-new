0cf09eb16c856fc929d067ed1e6e7c65efb22a87
Francesco Persico <francescopersico@gmail.com>
2020-03-09 15:58:44 +0100

    [#171701030] APIM additional modules (#63)

---
+++ azurerm_api_management_api/main.tf
@@ -0,0 +1,48 @@
+provider "azurerm" {
+  version = "=1.44.0"
+}
+
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "azurerm" {}
+}
+
+data "template_file" "swagger_json" {
+  template = var.swagger_json_template
+  vars = {
+    host = var.host
+  }
+}
+
+resource "azurerm_api_management_api" "api_management_api" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  api_management_name = var.api_management_name
+  revision            = var.revision
+  display_name        = var.display_name
+  description         = var.description
+  path                = var.path
+  protocols           = var.protocols
+
+  import {
+    content_format = "swagger-json"
+    content_value  = data.template_file.swagger_json.rendered
+  }
+}
+
+resource "azurerm_api_management_api_policy" "api_management_api_policy" {
+  api_name            = azurerm_api_management_api.api_management_api.name
+  api_management_name = var.api_management_name
+  resource_group_name = var.resource_group_name
+
+  xml_content = var.policy_xml
+}
+
+resource "azurerm_api_management_product_api" "api_management_product_api" {
+  for_each = toset(var.product_ids)
+
+  product_id          = each.value
+  api_name            = azurerm_api_management_api.api_management_api.name
+  api_management_name = var.api_management_name
+  resource_group_name = var.resource_group_name
+}
