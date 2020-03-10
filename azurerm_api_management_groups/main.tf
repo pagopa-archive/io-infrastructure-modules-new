e3cb8020bf1a668ce9d3731c03aa17ffcca40a9e
Francesco Persico <francescopersico@gmail.com>
2020-03-10 11:13:17 +0100

    [#171719953] Module api_management_groups (#65)

---
+++ azurerm_api_management_groups/main.tf
@@ -0,0 +1,23 @@
+provider "azurerm" {
+  version = "=1.44.0"
+}
+
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "azurerm" {}
+}
+
+resource "azurerm_api_management_group" "api_management_group" {
+  for_each = {
+    for group in var.groups :
+    group.name => {
+      name         = group.name
+      display_name = group.display_name
+    }
+  }
+
+  resource_group_name = var.resource_group_name
+  api_management_name = var.api_management_name
+  name                = each.value.name
+  display_name        = each.value.display_name
+}
---
+++ azurerm_api_management_groups/vars.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "api_management_name" {
+  type = string
+}
+
+variable "groups" {
+  type = list(any)
+}
