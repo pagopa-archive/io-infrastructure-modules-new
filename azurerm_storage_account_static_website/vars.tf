c0d0f3568d5252f97fae393735a533356dee9357
Roberto Chessa <r.chessa@beetobit.com>
2020-03-11 12:08:46 +0100

    [#171536959] first import

---
+++ azurerm_storage_account_static_website/vars.tf
@@ -0,0 +1,53 @@
+variable "global_prefix" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "environment_short" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "account_kind" {
+  type    = string
+  default = "StorageV2"
+}
+
+variable "account_tier" {
+  type = string
+}
+
+variable "account_replication_type" {
+  type = string
+}
+
+variable "access_tier" {
+  type = string
+}
+
+variable "index_document" {
+  type    = string
+}
+
+variable "error_404_document" {
+  type    = string
+  default = null
+}
+
+locals {
+  resource_name = "${var.global_prefix}${var.environment_short}st${var.name}"
+}
