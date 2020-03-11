35cc84ded75aa622a2f42bd715d89aa17be5ef62
Roberto Chessa <rchessa@users.noreply.github.com>
2020-03-11 12:49:01 +0100

    [#171536959] Module storage_account_static_website (#67)

---
+++ azurerm_storage_account_static_website/outputs.tf
@@ -0,0 +1,29 @@
+output "id" {
+  value = azurerm_storage_account.storage_account.id
+}
+
+output "primary_connection_string" {
+  value     = azurerm_storage_account.storage_account.primary_connection_string
+  sensitive = true
+}
+
+output "primary_access_key" {
+  value     = azurerm_storage_account.storage_account.primary_access_key
+  sensitive = true
+}
+
+output "primary_blob_host" {
+  value = azurerm_storage_account.storage_account.primary_blob_host
+}
+
+output "primary_web_host" {
+  value = azurerm_storage_account.storage_account.primary_web_host
+}
+
+output "primary_web_endpoint" {
+  value = azurerm_storage_account.storage_account.primary_web_endpoint
+}
+
+output "resource_name" {
+  value = local.resource_name
+}
