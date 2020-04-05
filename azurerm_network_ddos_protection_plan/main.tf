3817beb52ef4e1a1bb3608dccab905986f26d342
Francesco Persico <francescopersico@gmail.com>
2020-04-05 23:40:30 +0200

    [#172166442] Module network_ddos_protection_plan (#92)

---
+++ azurerm_network_ddos_protection_plan/main.tf
@@ -0,0 +1,19 @@
+provider "azurerm" {
+  version = "=2.4.0"
+  features {}
+}
+
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "azurerm" {}
+}
+
+resource "azurerm_network_ddos_protection_plan" "network_ddos_protection_plan" {
+  name                = local.resource_name
+  location            = var.region
+  resource_group_name = var.resource_group_name
+
+  tags = {
+    environment = var.environment
+  }
+}
