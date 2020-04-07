95dbd6e0297fb7f41b4ec14e09b95457502da4dd
Francesco Persico <francescopersico@gmail.com>
2020-04-07 09:02:34 +0200

    [#172195312] Add module app_service_slot (#93)

---
+++ azurerm_app_service_slot/vars.tf
@@ -0,0 +1,108 @@
+variable "global_prefix" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "environment_short" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "app_service_name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "app_service_plan_id" {
+  type = string
+}
+
+variable "app_enabled" {
+  type        = bool
+  description = "Is the App Service Enabled?"
+  default     = true
+}
+
+variable "https_only" {
+  type    = bool
+  default = true
+}
+
+variable "application_insights_instrumentation_key" {
+  type = string
+}
+
+variable "app_settings" {
+  type    = map(any)
+  default = {}
+}
+
+variable "app_settings_secrets" {
+  type = object({
+    key_vault_id = string
+    map          = map(string)
+  })
+}
+
+variable "always_on" {
+  type    = bool
+  default = true
+}
+
+variable "allowed_ips" {
+  // List of ip
+  type    = list(string)
+  default = []
+}
+
+variable "allowed_ips_secret" {
+  // List of ip from a keyvault secret
+  type = object({
+    key_vault_id     = string
+    key_vault_secret = string
+  })
+
+  default = null
+}
+
+variable "allowed_subnets" {
+  // List of subnet id
+  type    = list(string)
+  default = []
+}
+
+variable "subnet_id" {
+  type = string
+
+  default = null
+}
+
+variable "log_analytics_workspace_id" {
+  type        = string
+  description = "The log_analytics workspace id"
+  default     = null
+}
+
+variable "application_logs" {
+  type = object({
+    key_vault_id             = string
+    key_vault_secret_sas_url = string
+    level                    = string
+    retention_in_days        = number
+  })
+
+  default = null
+}
