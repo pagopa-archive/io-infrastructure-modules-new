fb30c31c4a701c46778989a013943fe05559b292
Walter Traspadini <walter.traspadini@pagopa.it>
2020-04-09 12:37:28 +0200

    [#172220829] Fix Association Eventhub Diagnostic Settings (#97)
    * [172220829] build the rule_id as string conctatenation
    
    * [#171536912] upgrade azure provider
    
    Co-authored-by: Walter Traspadini <walter.traspadini@sorint.it>

--- azurerm_monitor_diagnostic_setting/main.tf
+++ azurerm_monitor_diagnostic_setting/main.tf
@@ -8,14 +8,18 @@ terraform {
   backend "azurerm" {}
 }
 
+data "azurerm_subscription" "subscription" {
+}
+
 resource "azurerm_monitor_diagnostic_setting" "monitor_diagnostic_setting" {
   name                       = "${var.name}-diagnostic-setting"
   target_resource_id         = var.target_resource_id
   log_analytics_workspace_id = var.log_analytics_workspace_id
+  storage_account_id         = var.storage_account_id
 
-  eventhub_name                  = var.eventhub_name
-  eventhub_authorization_rule_id = var.eventhub_name == null ? null : var.eventhub_authorization_rule_id
-  storage_account_id             = var.storage_account_id
+  eventhub_name = var.eventhub_name
+  # required due to this issue: https://github.com/terraform-providers/terraform-provider-azurerm/issues/6356
+  eventhub_authorization_rule_id = var.eventhub_authorization_rule != null ? "/subscriptions/${data.azurerm_subscription.subscription.subscription_id}/resourceGroups/${var.eventhub_resource_group_name}/providers/Microsoft.EventHub/namespaces/${var.eventhub_namespace_name}/authorizationrules/${var.eventhub_authorization_rule}" : null
 
   # Note: retention_policies are still required even if it is not useful 
   #       when we use the log analytics workspace.  
