ac0d06f90c2c2942f0ab46fb42f641b40efc1e9c
Walter Traspadini <walter.traspadini@sorint.it>
2020-04-15 18:01:53 +0200

    [#172335576]-azurerm_log_analytics_workspace-missed-block-features

--- azurerm_log_analytics_workspace/outputs.tf
+++ azurerm_log_analytics_workspace/outputs.tf
@@ -9,7 +9,7 @@ output "id" {
 }
 
 output "primary_shared_key" {
-  description = "The Primary shared key for the Log Analytics Workspace." 
+  description = "The Primary shared key for the Log Analytics Workspace."
   value       = azurerm_log_analytics_workspace.log_analytics_workspace.primary_shared_key
   sensitive   = true
 }
