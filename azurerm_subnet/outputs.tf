a78d870d5aa04a1b848c69ab041f53c93a065e82
Francesco Persico <francescopersico@gmail.com>
2020-04-16 17:25:10 +0200

    [#172357479] Module subnet export address_prefix (#113)

--- azurerm_subnet/outputs.tf
+++ azurerm_subnet/outputs.tf
@@ -5,3 +5,7 @@ output "id" {
 output "name" {
   value = var.module_disabled ? null : azurerm_subnet.subnet[0].name
 }
+
+output "address_prefix" {
+  value = var.module_disabled ? null : azurerm_subnet.subnet[0].address_prefix
+}
