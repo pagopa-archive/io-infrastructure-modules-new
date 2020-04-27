3e83ebb0d80ad727c039e755a75012b65f3901ef
Walter Traspadini <walter.traspadini@sorint.it>
2020-04-27 16:43:51 +0200

    [#172530138] Fix missed provider declaration

--- azurerm_dns_caa_record/main.tf
+++ azurerm_dns_caa_record/main.tf
@@ -1,3 +1,12 @@
+provider "azurerm" {
+  version = "=2.4.0"
+  features {}
+}
+
+terraform {
+  backend "azurerm" {}
+}
+
 resource "azurerm_dns_caa_record" "dns_caa_record" {
   name                = var.name
   zone_name           = var.zone_name
