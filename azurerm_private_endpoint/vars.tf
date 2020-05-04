0647c097f7e3a4b0a3263732e19a501ab6b48dc6
Walter Traspadini <walter.traspadini@sorint.it>
2020-05-04 16:57:58 +0200

    [#172647975] New module azurerm_private_endpoint

---
+++ azurerm_private_endpoint/vars.tf
@@ -0,0 +1,40 @@
+# General variables
+
+variable "global_prefix" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "environment_short" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "The ID of the Subnet from which Private IP Addresses will be allocated for this Private Endpoint."
+}
+
+variable "private_service_connection" {
+  type = object({
+    name                           = string
+    private_connection_resource_id = string
+    is_manual_connection           = bool
+    subresource_names              = list(string)
+    }
+  )
+}
