96703dea13b45d2db082499b069efb60e141f7ee
Walter Traspadini <walter.traspadini@sorint.it>
2020-05-12 10:54:34 +0200

    [#172773196] Add private ip address in the output

--- azurerm_private_endpoint/outputs.tf
+++ azurerm_private_endpoint/outputs.tf
@@ -16,3 +16,8 @@ output "private_dns_zone_name" {
   description = "The name of the privae dns zone."
   value       = module.private_dns_zone.*.name
 }
+
+output "private_ip_address" {
+  value = azurerm_private_endpoint.private_endpoint.private_service_connection.*.private_ip_address
+  description = "The private ip addresses associated to the endpoint"
+}
