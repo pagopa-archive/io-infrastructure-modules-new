dae0309aaa80e00ee07637ce17db2fdaa9cb1be9
Walter Traspadini <walter.traspadini@sorint.it>
2020-05-13 15:33:47 +0200

    [#172817813] Add private_id_address in output

--- azurerm_private_endpoint/outputs.tf
+++ azurerm_private_endpoint/outputs.tf
@@ -6,3 +6,8 @@ output "name" {
   description = "The name of the private endpoint."
   value       = azurerm_private_endpoint.private_endpoint.name
 }
+
+output "private_ip_address" {
+  value       = azurerm_private_endpoint.private_endpoint.private_service_connection.*.private_ip_address
+  description = "The private ip addresses associated to the endpoint"
+}
