96c5dad10b6003d15de748d918e2e6dd44e02438
Walter Traspadini <walter.traspadini@sorint.it>
2020-05-15 15:49:11 +0200

    [#172856324] Let filtes and snapshot to be options

--- azurerm_storage_management_policy/main.tf
+++ azurerm_storage_management_policy/main.tf
@@ -18,9 +18,12 @@ resource "azurerm_storage_management_policy" "storage_management_policy" {
       name    = rule.value.name
       enabled = rule.value.enabled
 
-      filters {
-        prefix_match = rule.value.filters.prefix_match
-        blob_types   = rule.value.filters.blob_types
+      dynamic "filters" {
+        for_each = rule.value.filters == null ? [] : list(rule.value.filters)
+        content {
+          prefix_match = filters.value.prefix_match
+          blob_types   = filters.value.blob_types
+        }
       }
 
       actions {
@@ -30,7 +33,7 @@ resource "azurerm_storage_management_policy" "storage_management_policy" {
           delete_after_days_since_modification_greater_than          = rule.value.actions.base_blob.delete_after_days_since_modification_greater_than
         }
         dynamic "snapshot" {
-          for_each = rule.value.actions.snapshot == null ? [] : [rule.value.actions.snapshot]
+          for_each = rule.value.actions.snapshot == null ? [] : list(rule.value.actions.snapshot)
           content {
             delete_after_days_since_creation_greater_than = snapshot.value.delete_after_days_since_creation_greater_than
           }
