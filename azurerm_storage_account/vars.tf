f6dd9b17eac709593b64d532759bba5e40aee91d
Pasquale De Vita <pasqualedevita@Pasquales-MacBook-Pro.local>
2020-05-15 17:55:20 +0200

    indent lint

--- azurerm_storage_account/vars.tf
+++ azurerm_storage_account/vars.tf
@@ -40,9 +40,9 @@ variable "access_tier" {
 }
 
 variable "blob_properties_delete_retention_policy_days" {
-  type = number
+  type        = number
   description = "Enable soft delete policy and specify the number of days that the blob should be retained"
-  default = null
+  default     = null
 }
 
 locals {
