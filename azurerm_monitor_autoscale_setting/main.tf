5936cdf51ae51f05b4ce27c40c68e8e682936aba
Walter Traspadini <walter.traspadini@pagopa.it>
2020-05-27 15:19:16 +0200

    [#173010454] Module azurerm_monitor_autoscale_setting (#135)
    * [#173010454] Draft first commit
    
    * [#173010454] Add list of prifiles with rules
    
    * [#173010454] toset func in for_each rules
    
    * [#173010454] Changed list to map in profile iteration to grant order positions
    
    * [#173010454] Introduced name for rules only for sorting.
    
    * [#173010454] Review change request: var from string to bool
    
    Co-authored-by: Walter Traspadini <walter.traspadini@sorint.it>

---
+++ azurerm_monitor_autoscale_setting/main.tf
@@ -0,0 +1,84 @@
+provider "azurerm" {
+  version = "=2.11.0"
+  features {}
+}
+
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "azurerm" {}
+}
+
+resource "azurerm_monitor_autoscale_setting" "monitor_autoscale_setting" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.region
+  target_resource_id  = var.target_resource_id
+
+  dynamic "profile" {
+    for_each = { for p in var.profiles : p.name => p }
+    content {
+      name = profile.key
+      capacity {
+        default = profile.value.capacity.default
+        minimum = profile.value.capacity.minimum
+        maximum = profile.value.capacity.maximum
+      }
+
+      dynamic "rule" {
+        for_each = { for r in profile.value.rules : r.name => r }
+        content {
+          metric_trigger {
+            metric_name        = rule.value.metric_trigger.metric_name
+            metric_resource_id = rule.value.metric_trigger.metric_resource_id
+            time_grain         = rule.value.metric_trigger.time_grain
+            statistic          = rule.value.metric_trigger.statistic
+            time_window        = rule.value.metric_trigger.time_window
+            time_aggregation   = rule.value.metric_trigger.time_aggregation
+            operator           = rule.value.metric_trigger.operator
+            threshold          = rule.value.metric_trigger.threshold
+          }
+          scale_action {
+            direction = rule.value.scale_action.direction
+            type      = rule.value.scale_action.type
+            value     = rule.value.scale_action.value
+            cooldown  = rule.value.scale_action.cooldown
+          }
+        }
+      }
+
+      dynamic "fixed_date" {
+        for_each = profile.value.fixed_date != null ? [profile.value.fixed_date] : []
+        content {
+          timezone = fixed_date.value.timezone
+          start    = fixed_date.value.start
+          end      = fixed_date.value.end
+        }
+      }
+
+      dynamic "recurrence" {
+        for_each = profile.value.recurrence != null ? [profile.value.recurrence] : []
+        content {
+          timezone = recurrence.value.timezone
+          days     = recurrence.value.days
+          hours    = recurrence.value.hours
+          minutes  = recurrence.value.minutes
+        }
+      }
+    }
+  }
+
+  dynamic "notification" {
+    for_each = var.notification != null ? [var.notification] : []
+    content {
+      email {
+        send_to_subscription_administrator    = notification.value.email.send_to_subscription_administrator
+        send_to_subscription_co_administrator = notification.value.email.send_to_subscription_co_administrator
+        custom_emails                         = notification.value.email.custom_emails
+      }
+    }
+  }
+
+  tags = {
+    environment = var.environment
+  }
+}
---
+++ azurerm_monitor_autoscale_setting/vars.tf
@@ -0,0 +1,92 @@
+# General variables
+
+variable "global_prefix" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "environment_short" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "name" {
+  type        = string
+  description = "The name of the AutoScale Setting."
+}
+
+variable "target_resource_id" {
+  type        = string
+  description = "The name of the Resource Group in the AutoScale Setting should be created."
+}
+
+variable "profiles" {
+  type = list(object(
+    {
+      name = string
+      capacity = object({
+        # between 0 and 1000.
+        default = number
+        # between 0 and 1000.
+        maximum = number
+        # between 0 and 1000.
+        minimum = number
+      })
+      rules = list(object({
+        name = string # it should be unique among rules ensure sorting.
+        metric_trigger = object({
+          metric_name        = string
+          metric_resource_id = string
+          operator           = string
+          statistic          = string
+          time_aggregation   = string
+          time_grain         = string
+          time_window        = string
+          threshold          = string
+        })
+        scale_action = object({
+          cooldown  = string
+          direction = string
+          type      = string
+          value     = number
+        })
+
+      }))
+
+      fixed_date = object({
+        end      = string
+        start    = string
+        timezone = string
+
+      })
+
+      recurrence = object({
+        timezone = string
+        days     = list(string)
+        hours    = list(string)
+        minutes  = list(string)
+      })
+    })
+  )
+}
+
+variable "notification" {
+  type = object({
+    email = object({
+      send_to_subscription_administrator    = bool
+      send_to_subscription_co_administrator = bool
+      custom_emails                         = list(string)
+    })
+  })
+  default = null
+}
