9a8eb8b6369b721c77cab9011095bd882d344c1f
Walter Traspadini <walter.traspadini@pagopa.it>
2020-06-01 10:26:03 +0200

    [#172935200] Fix Address_prefix deprecate in subnet module (#137)
    * [#172935200] replace addess_prefix with address prefixes
    
    * [#172935200] removed comment
    
    Co-authored-by: Walter Traspadini <walter.traspadini@sorint.it>

--- azurerm_subnet/main.tf
+++ azurerm_subnet/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = var.name
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network_name
-  address_prefix       = var.address_prefix
+  address_prefixes = concat(var.address_prefixes, var.address_prefix != null ? [var.address_prefix] : [])
 
   dynamic "delegation" {
     for_each = var.delegation == null ? [] : ["delegation"]
