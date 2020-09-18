85228f81363a0d199d115380e86d988ed04584d3
Walter Traspadini <walter.traspadini@sorint.it>
2020-09-18 12:18:27 +0200

    removed the backend block from virtual network module

--- azurerm_virtual_network/main.tf
+++ azurerm_virtual_network/main.tf
@@ -3,11 +3,6 @@ provider "azurerm" {
   features {}
 }
 
-terraform {
-  # The configuration for this backend will be filled in by Terragrunt
-  backend "azurerm" {}
-}
-
 resource "azurerm_virtual_network" "virtual_network" {
   name                = local.resource_name
   resource_group_name = var.resource_group_name
