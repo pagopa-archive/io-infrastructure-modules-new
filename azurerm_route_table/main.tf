110a83bcecbd099b1cdbd2d2e4870011066ee0ff
Walter Traspadini <walter.traspadini@pagopa.it>
2020-11-11 15:17:20 +0100

    [#175681501] module azurerm route table (#179)
    * [#175681501] module azurerm_route_table first commit
    
    * [#175681501] fix missed next_hop_in_ip_address

--- azurerm_route_table/main.tf
+++ azurerm_route_table/main.tf
@@ -12,9 +12,10 @@ resource "azurerm_route_table" "route_table" {
   dynamic "route" {
     for_each = var.routes
     content {
-      name           = route.value.name
-      address_prefix = route.value.address_prefix
-      next_hop_type  = route.value.next_hop_type
+      name                   = route.value.name
+      address_prefix         = route.value.address_prefix
+      next_hop_type          = route.value.next_hop_type
+      next_hop_in_ip_address = route.value.next_hop_in_ip_address
     }
   }
 
