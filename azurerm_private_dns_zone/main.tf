38e3d684c3bd2f87e20c42c6ca0ecbc6736faa53
Walter Traspadini <walter.traspadini@pagopa.it>
2020-11-24 12:06:00 +0100

    [#175853014] Private dns a record within the zone (#182)
    * [#175853014] module azurerm_dns_a_record
    
    * [#175853014] create private dns a records inside private dns zone

--- azurerm_private_dns_zone/main.tf
+++ azurerm_private_dns_zone/main.tf
@@ -10,3 +10,19 @@ resource "azurerm_private_dns_zone" "private_dns_zone" {
     environment = var.environment
   }
 }
+
+
+module "azurerm_private_dns_a_record" {
+  count  = length(var.dns_a_records)
+  source = "git::git@github.com:pagopa/io-infrastructure-modules-new.git//azurerm_private_dns_a_record?ref=v2.1.12"
+
+  name                = var.dns_a_records[count.index].name
+  zone_name           = azurerm_private_dns_zone.private_dns_zone.name
+  resource_group_name = var.resource_group_name
+
+  ttl     = var.dns_a_records[count.index].ttl
+  records = var.dns_a_records[count.index].records
+
+  environment = var.environment
+
+}
