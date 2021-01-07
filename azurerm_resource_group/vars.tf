986d81d688f2a0ff708cfe7cdf2ecfe6f6fcd6a2
Walter Traspadini <walter.traspadini@sorint.it>
2021-01-07 09:59:54 +0100

    [#176392355] variable resource_group_name to overwrite

--- azurerm_resource_group/vars.tf
+++ azurerm_resource_group/vars.tf
@@ -15,9 +15,16 @@ variable "region" {
 }
 
 variable "name" {
-  type = string
+  type    = string
+  default = null
+
+}
+variable "resource_group_name" {
+  type        = string
+  description = "DEPRECATED: resource group name without prefix and environment. Use just name whenever it is possible."
+  default     = null
 }
 
 locals {
-  resource_name = "${var.global_prefix}-${var.environment_short}-rg-${var.name}"
+  resource_name = var.resource_group_name != null ? var.resource_group_name : "${var.global_prefix}-${var.environment_short}-rg-${var.name}"
 }
