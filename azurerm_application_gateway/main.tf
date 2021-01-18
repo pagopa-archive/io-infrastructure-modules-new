8737df16c9a906bf0cc12a854aceafcc4c8d21ee
pp-ps <63303456+pp-ps@users.noreply.github.com>
2021-01-18 11:59:15 +0100

    remove cbc ciphers (#201)

--- azurerm_application_gateway/main.tf
+++ azurerm_application_gateway/main.tf
@@ -219,9 +219,7 @@ resource "azurerm_application_gateway" "application_gateway" {
   ssl_policy {
     policy_type = "Custom"
     cipher_suites = [
-      "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
       "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
-      "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
       "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
     ]
     min_protocol_version = "TLSv1_2"
