f1fbffb5f590164441d84e6d492727d0e5abeb5b
pp-ps <pietro.stroia@pagopa.it>
2021-01-19 09:32:26 +0100

    reenable cbc ciphers for some 4.4.x Android clients

--- azurerm_application_gateway/main.tf
+++ azurerm_application_gateway/main.tf
@@ -220,7 +220,9 @@ resource "azurerm_application_gateway" "application_gateway" {
     policy_type = "Custom"
     cipher_suites = [
       "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256",
-      "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384"
+      "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384",
+      "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
+      "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA"
     ]
     min_protocol_version = "TLSv1_2"
   }
