d0f9b028e97b00e1ed79b533ebcabea6f0ba58a1
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2021-01-21 09:23:02 +0100

    [#176578676] Add azurerm_application_insights_web_test module (#203)
    * [#176578676] Add azurerm_application_insights_web_test module
    
    * fix description
    
    * added optional http_method and http_status_code
    
    * fix typo
    
    Co-authored-by: pasqualedevita <>

---
+++ azurerm_application_insights_web_test/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "azurerm" {}
+}
+
+resource "azurerm_application_insights_web_test" "application_insights_web_test" {
+  name                    = local.web_test_resource_name
+  resource_group_name     = var.resource_group_name
+  location                = var.region
+  application_insights_id = var.application_insights_id
+
+  kind          = var.kind
+  frequency     = var.frequency
+  timeout       = var.timeout
+  enabled       = var.enabled
+  retry_enabled = var.retry_enabled
+  geo_locations = var.geo_locations
+  configuration = templatefile(var.configuration, { name = local.web_test_resource_name,
+                                                    url = var.url,
+                                                    timeout = var.timeout,
+                                                    http_method = var.http_method,
+                                                    http_status_code = var.http_status_code, })
+
+  tags = {
+    environment                                 = var.environment
+    # https://github.com/terraform-providers/terraform-provider-azurerm/issues/7026
+    # azurerm_application_insights_web_test creates hidden tag that always reflects a "change"
+    # A reference to a resource type must be followed by at least one attribute access, specifying the resource name.
+    # "hidden-link:${var.application_insights_id}" = Resource
+  }
+
+  # suggestion to avoid azurerm_application_insights_web_test hidden-link changes
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
---
+++ azurerm_application_insights_web_test/outputs.tf
@@ -0,0 +1,7 @@
+output "name" {
+  value = azurerm_application_insights_web_test.application_insights_web_test.name
+}
+
+output "id" {
+  value = azurerm_application_insights_web_test.application_insights_web_test.id
+}
