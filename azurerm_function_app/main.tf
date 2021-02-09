07072d697bb9c973c9dae024feb0cc1be208a36e
Walter Traspadini <walter.traspadini@pagopa.it>
2021-02-09 10:54:10 +0100

    [#176863396] removed web test module. (#208)

--- azurerm_function_app/main.tf
+++ azurerm_function_app/main.tf
@@ -167,21 +167,3 @@ resource "azurerm_app_service_virtual_network_swift_connection" "app_service_vir
   app_service_id = azurerm_function_app.function_app.id
   subnet_id      = var.subnet_id != null ? var.subnet_id : module.subnet[0].id
 }
-
-module "application_insights_web_test" {
-  count  = var.web_test != null && var.health_check_path != null ? 1 : 0
-  source = "git::git@github.com:pagopa/io-infrastructure-modules-new.git//azurerm_function_app?ref=v2.1.29"
-
-  global_prefix     = var.global_prefix
-  environment       = var.environment
-  environment_short = var.environment_short
-  region            = var.region
-
-  name                    = "${var.resources_prefix.function_app}-${var.name}"
-  resource_group_name     = var.resource_group_name
-  application_insights_id = var.web_test.application_insights_id
-
-  kind    = "ping"
-  enabled = var.web_test.enabled
-  url     = "https://${azurerm_function_app.function_app.default_hostname}${var.health_check_path}"
-}
