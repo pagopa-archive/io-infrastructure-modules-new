81dc2bd38d602fe2d11d8082a4ea1ab7f9cf2851
Walter Traspadini <walter.traspadini@sorint.it>
2021-04-12 12:15:59 +0200

    [#IOINFRA-58] new module azurerm_api_management_api_operation_policy

---
+++ azurerm_api_management_api_operation_policy/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 0.14.5"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.46.1"
+    }
+  }
+  backend "azurerm" {}
+}
+
+resource "azurerm_api_management_api_operation_policy" "this" {
+  api_name            = var.api_name
+  api_management_name = var.api_management_name
+  resource_group_name = var.resource_group_name
+  operation_id        = var.operation_id
+
+  xml_content = var.xml_content
+
+}
