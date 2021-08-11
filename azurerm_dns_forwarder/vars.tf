6fa672e8bd625013e317d0a6391142cc90da01ea
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2021-08-11 19:40:52 +0200

    [#IOINFRA-178] Add dns forwarder module (#223)
    Co-authored-by: pasqualedevita <>

---
+++ azurerm_dns_forwarder/vars.tf
@@ -0,0 +1,29 @@
+# General variables
+
+variable "global_prefix" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "environment_short" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
