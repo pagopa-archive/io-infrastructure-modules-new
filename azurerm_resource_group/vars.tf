def75bc6c82b3ef91feb5a3e96512dba6f3c4af5
pasqualedevita <>
2021-08-11 10:46:05 +0200

    revert

--- azurerm_resource_group/vars.tf
+++ azurerm_resource_group/vars.tf
@@ -19,6 +19,12 @@ variable "name" {
   default = null
 }
 
+variable "resource_group_name" {
+  type        = string
+  description = "DEPRECATED: resource group name without prefix and environment. Use just name whenever it is possible."
+  default     = null
+}
+
 locals {
   resource_name = var.resource_group_name != null ? var.resource_group_name : "${var.global_prefix}-${var.environment_short}-rg-${var.name}"
 }
