e6ea0526a108e368d2d160e7e637b1e86ad99f34
Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>
2021-08-11 12:19:55 +0200

    Add secrets parameters to logic app (#222)
    Co-authored-by: pasqualedevita <>

--- azurerm_logic_app/main.tf
+++ azurerm_logic_app/main.tf
@@ -14,5 +14,15 @@ resource "azurerm_logic_app_workflow" "logic_app" {
   name                = local.resource_name
   resource_group_name = var.resource_group_name
   location            = var.region
-  parameters          = var.parameters
+  parameters = merge(
+    var.parameters,
+    module.secrets_from_keyvault.secrets_with_value
+  )
+}
+
+module "secrets_from_keyvault" {
+  source = "git::git@github.com:pagopa/io-infrastructure-modules-new.git//azurerm_secrets_from_keyvault?ref=v3.0.3"
+
+  key_vault_id = var.parameters_secrets.key_vault_id
+  secrets_map  = var.parameters_secrets.map
 }
