d1c82d9b6f27e27a43d200e04f2c0dc0fb3b0c00
uolter <walter.traspadini@pagopa.it>
2021-11-26 16:05:25 +0100

    updated all modules to next release 4.0.0

--- azurerm_function_app_slot/main.tf
+++ azurerm_function_app_slot/main.tf
@@ -18,7 +18,7 @@ data "azurerm_key_vault_secret" "allowed_ips_secret" {
 }
 
 module "secrets_from_keyvault" {
-  source = "git::git@github.com:pagopa/io-infrastructure-modules-new.git//azurerm_secrets_from_keyvault?ref=v3.0.11"
+  source = "git::git@github.com:pagopa/io-infrastructure-modules-new.git//azurerm_secrets_from_keyvault?ref=v4.0.0"
 
   key_vault_id = var.app_settings_secrets.key_vault_id
   secrets_map  = var.app_settings_secrets.map
--- azurerm_logic_app/main.tf
+++ azurerm_logic_app/main.tf
@@ -21,7 +21,7 @@ resource "azurerm_logic_app_workflow" "logic_app" {
 }
 
 module "secrets_from_keyvault" {
-  source = "git::git@github.com:pagopa/io-infrastructure-modules-new.git//azurerm_secrets_from_keyvault?ref=v3.0.11"
+  source = "git::git@github.com:pagopa/io-infrastructure-modules-new.git//azurerm_secrets_from_keyvault?ref=v4.0.0"
 
   key_vault_id = var.parameters_secrets.key_vault_id
   secrets_map  = var.parameters_secrets.map
